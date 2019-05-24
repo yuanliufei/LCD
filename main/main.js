4d049232f4a4c5d097483eb23d79445f7ae65a6c
faker <2975777969@qq.com>
2019-05-24 14:36:44 +0800

    if input string

--- main/main.js
+++ main/main.js
@@ -1,4 +1,26 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(a) {
+  //var top=["._.","...","._.","._.","...","._.","._.","._.","._.","._."];	//top style
+  var middle=["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];	//middle style
+  var bottom=["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];	// bottom style
+  var tops=["._.","...","._.","._.","...","._.","._.","._.","._.","._."];	//top style
+ //var a="910";
+  var length=a.length;
+  var array=new Array();
+  for(var k=0;k<3;k++){			
+    array[k]=new Array();
+  }
+  for(var k=0;k<length;k++){
+   	var tmp=a[k];
+    array[0].push(tops[tmp]);
+    array[1].push(middle[tmp]);
+    array[2].push(bottom[tmp]);
+  }
+  for(var k=0;k<3;k++){			
+    array[k].push('\n');
+  }
+  //var str=array[0].join(' ')+"<br>"+array[1].join(' ')+"<br>"+array[2].join(' ');
+  var strs=array[0].join(' ')+array[1].join(' ')+array[2].join(' ');
+	//document.getElementById("demo").innerHTML=str;
+	console.log(strs);
+	//return array[0].join(' ');
 };
